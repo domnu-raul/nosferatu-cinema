bb7be6b37586ba35b19cfed1f4076225e8b41ba6
domnu-raul <73673625+domnu-raul@users.noreply.github.com>
2023-06-07 15:23:57 +0300

    json data

--- functions/sign-in.script.php
+++ functions/sign-in.script.php
@@ -14,7 +14,7 @@ if (empty($username) || empty($password)) {
     exit();
 }
 
-include('../includes/database.inc.php');
+require('../includes/database.inc.php');
 
 $sql = "SELECT * FROM users WHERE username = '{$username}'";
 $result = $conn->query($sql);
